a94311d5a93f0e7a7d7ec05cc5aa828a51f90a40
Pablo Lopes <149916584+PollarZzz@users.noreply.github.com>
2024-03-13 02:58:35 -0300

    Add files via upload

---
+++ Exercício 2.py
@@ -0,0 +1,17 @@
+# Ex 2
+
+print("Vamos brincar de somar.")
+n1 = int(input('Digite o valor: '))
+n2 = int(input('Digite o valor: '))
+s = n1 + n2
+
+print(f'A soma entre {n1} e {n2} corresponde a {s}')
+
+input("\n Pressione ENTER para sair")
+
+#    - Tipos primitivos no Python:
+#    * Int (Inteiro) 1 / 2 / 3 / 4...
+#    * Float (Número de ponto flutuante) 1.1 / 2.2 / 3.3 / 4.4...
+#    * Complex (Número complexo) *Não entendi ainda*
+#    * Bool (Booleano) False / True
+#    * String (Texto) Auto explicativo...
