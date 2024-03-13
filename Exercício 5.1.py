e4f3c8e82e1ea675947208e4a881db247b5f884f
Pablo Lopes <149916584+PollarZzz@users.noreply.github.com>
2024-03-13 03:03:35 -0300

    Add files via upload

---
+++ Exercício 5.1.py
@@ -0,0 +1,12 @@
+# Ex 05
+# Parte 1/3
+
+# Calcule a raiz quadrada de um número
+
+print('Vamos calcular algumas raizes quadradas!!\n ')
+
+operacao = int(input('Raiz quadrada de: '))
+total = operacao ** (1/2)
+print(f'O resultado da Raiz Quadrada de {operacao} é {total}')
+
+input("\n Pressione ENTER para sair")
