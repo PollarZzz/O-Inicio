df4d3522893aba3ae1648f35bf145a95eb8ee2f0
Pablo Lopes <149916584+PollarZzz@users.noreply.github.com>
2024-03-13 03:03:48 -0300

    Add files via upload

---
+++ Exercício 5.2.py
@@ -0,0 +1,13 @@
+# Ex 05
+# Parte 2/3
+
+# Calcule a raiz cúbica de um número
+
+print('Vamos calcular algumas raizes cúbicas!!\n ')
+
+operacao = int(input('Qual a Raiz Cúbica de: '))
+total = operacao ** (1/3)
+
+print(f'A raiz cúbica de {operacao} é {total}')
+
+input("\n Pressione ENTER para sair")
