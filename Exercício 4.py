2536bfbd412fe62b1f3713dd9dd600b35e7649df
Pablo Lopes <149916584+PollarZzz@users.noreply.github.com>
2024-03-13 03:00:37 -0300

    Add files via upload

---
+++ Exercício 4.py
@@ -0,0 +1,12 @@
+# Ex 4
+
+print("Aqui te apresento uma brincadeira, vamos saber o sucessor e antecessor\ndo número que digitar ^-^")
+
+numero = int(input('Entre com um número de sua escolha: '))
+
+resultado1 = numero + 1
+resultado2 = numero - 1
+
+print(f'O sucessor de {numero} é {resultado1} e o antecessor é {resultado2}')
+
+input("\n Pressione ENTER para sair")
