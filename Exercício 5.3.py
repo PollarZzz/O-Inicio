0f190e5830aa8ce4ba612ce5fc50ed9cd892c78c
Pablo Lopes <149916584+PollarZzz@users.noreply.github.com>
2024-03-13 03:04:00 -0300

    Add files via upload

---
+++ Exercício 5.3.py
@@ -0,0 +1,19 @@
+# Ex 05
+# Parte 3/3
+
+
+
+print("Vamos calcular o resultado da soma, da subtração e da da mutiplicação.\n ")
+n1 = int(input('Digite o primeiro valor númerico: '))
+n2 = int(input('Digite o segundo valor númerico: '))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+divisao_inteira = n1 // n2
+resto_da_divisao = n1 % n2
+
+print(f'O resultado da soma é: {soma}.\nO resultado da subtração é: {subtracao}.\nO resultado da mutiplicação é {multiplicacao}!!')
+
+input("\n Pressione ENTER para sair")
